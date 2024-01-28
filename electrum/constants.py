e2e3bd0849440f2de338c69959d12ce68bfceee6
BlackPeter13 <peterswebdesign2010@gmail.com>
2024-01-29 00:49:10 +0100

    Add files via upload

---
+++ electrum/constants.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+#
+# Electrum - lightweight Bitcoin client
+# Copyright (C) 2018 The Electrum developers
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import json
+
+from .util import inv_dict, all_subclasses
+from . import bitcoin
+
+
+def read_json(filename, default):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    try:
+        with open(path, 'r') as f:
+            r = json.loads(f.read())
+    except Exception:
+        r = default
+    return r
+
+
+GIT_REPO_URL = "https://github.com/bitcoin-atom/electrum"
+GIT_REPO_ISSUES_URL = "https://github.com/bitcoin-atom/electrum/issues"
+BIP39_WALLET_FORMATS = read_json('bip39_wallet_formats.json', [])
+
+
+class AbstractNet:
+
+    NET_NAME: str
+    TESTNET: bool
+    WIF_PREFIX: int
+    ADDRTYPE_P2PKH: int
+    ADDRTYPE_P2SH: int
+    SEGWIT_HRP: str
+    BOLT11_HRP: str
+    GENESIS: str
+    BLOCK_HEIGHT_FIRST_LIGHTNING_CHANNELS: int = 0
+    BIP44_COIN_TYPE: int
+    LN_REALM_BYTE: int
+
+    @classmethod
+    def max_checkpoint(cls) -> int:
+        return max(0, len(cls.CHECKPOINTS) * 2016 - 1)
+
+    @classmethod
+    def rev_genesis_bytes(cls) -> bytes:
+        return bytes.fromhex(bitcoin.rev_hex(cls.GENESIS))
+
+
+class BitcoinAtomMainnet(AbstractNet):
+
+    NET_NAME = "mainnet"
+    TESTNET = False
+    WIF_PREFIX = 0x80
+    ADDRTYPE_P2PKH = 23
+    ADDRTYPE_P2SH = 10
+    SEGWIT_HRP = "bca"
+    BOLT11_HRP = SEGWIT_HRP
+    GENESIS = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+    DEFAULT_PORTS = {'t': '50001', 's': '50002'}
+    DEFAULT_SERVERS = read_json('servers.json', {})
+    CHECKPOINTS = read_json('checkpoints.json', [])
+    POW_HEIGHT_CHANGE_CACHE = read_json('powheightchange.json', {})
+    BLOCK_HEIGHT_FIRST_LIGHTNING_CHANNELS = 497000
+    FORK_HEIGHT = 505888
+    BCA_HEIGHT = 505888
+    BCA_INIT_LIM = 50000
+    POW_LIMIT = 0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    POW_LIMIT_START = 0x000001ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    INITIAL_HASH_TARGET_POS = 0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    POW_TARGET_TIMESPAN = 14 * 24 * 60 * 60 # two weeks
+    POW_TARGET_SPACING = 10 * 60
+    POS_TARGET_TIMESPAN = 14 * 24 * 60 * 60  # two weeks
+    POS_TARGET_SPACING = 10 * 60
+    NEW_DIFFICULTY_ADJUSTMENT_ALGO_HEIGHT = 589500
+    POW_AVERAGING_WINDOW = 11
+    POW_ALLOW_MIN_DIFFICULTY_BLOCKS = False
+    POW_NO_RETARGETING = False
+
+    XPRV_HEADERS = {
+        'standard':    0x0488ade4,  # xprv
+        'p2wpkh-p2sh': 0x049d7878,  # yprv
+        'p2wsh-p2sh':  0x0295b005,  # Yprv
+        'p2wpkh':      0x04b2430c,  # zprv
+        'p2wsh':       0x02aa7a99,  # Zprv
+    }
+    XPRV_HEADERS_INV = inv_dict(XPRV_HEADERS)
+    XPUB_HEADERS = {
+        'standard':    0x0488b21e,  # xpub
+        'p2wpkh-p2sh': 0x049d7cb2,  # ypub
+        'p2wsh-p2sh':  0x0295b43f,  # Ypub
+        'p2wpkh':      0x04b24746,  # zpub
+        'p2wsh':       0x02aa7ed3,  # Zpub
+    }
+    XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
+    BIP44_COIN_TYPE = 0
+    LN_REALM_BYTE = 0
+    LN_DNS_SEEDS = [
+        'nodes.lightning.directory.',
+        'lseed.bitcoinstats.com.',
+        'lseed.darosior.ninja',
+    ]
+
+
+class BitcoinAtomTestnet(AbstractNet):
+
+    NET_NAME = "testnet"
+    TESTNET = True
+    WIF_PREFIX = 0xef
+    ADDRTYPE_P2PKH = 111
+    ADDRTYPE_P2SH = 196
+    SEGWIT_HRP = "tbca"
+    BOLT11_HRP = SEGWIT_HRP
+    GENESIS = "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
+    DEFAULT_PORTS = {'t': '51001', 's': '51002'}
+    DEFAULT_SERVERS = read_json('servers_testnet.json', {})
+    CHECKPOINTS = read_json('checkpoints_testnet.json', [])
+    POW_HEIGHT_CHANGE_CACHE = read_json('powheightchange_testnet.json', {})
+    FORK_HEIGHT = 1260000
+
+    XPRV_HEADERS = {
+        'standard':    0x04358394,  # tprv
+        'p2wpkh-p2sh': 0x044a4e28,  # uprv
+        'p2wsh-p2sh':  0x024285b5,  # Uprv
+        'p2wpkh':      0x045f18bc,  # vprv
+        'p2wsh':       0x02575048,  # Vprv
+    }
+    XPRV_HEADERS_INV = inv_dict(XPRV_HEADERS)
+    XPUB_HEADERS = {
+        'standard':    0x043587cf,  # tpub
+        'p2wpkh-p2sh': 0x044a5262,  # upub
+        'p2wsh-p2sh':  0x024289ef,  # Upub
+        'p2wpkh':      0x045f1cf6,  # vpub
+        'p2wsh':       0x02575483,  # Vpub
+    }
+    XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
+    BIP44_COIN_TYPE = 1
+    LN_REALM_BYTE = 1
+    LN_DNS_SEEDS = [  # TODO investigate this again
+        #'test.nodes.lightning.directory.',  # times out.
+        #'lseed.bitcoinstats.com.',  # ignores REALM byte and returns mainnet peers...
+    ]
+
+
+NETS_LIST = tuple(all_subclasses(AbstractNet))
+
+# don't import net directly, import the module instead (so that net is singleton)
+net = BitcoinAtomMainnet
+
+def set_mainnet():
+    global net
+    net = BitcoinAtomMainnet
+
+def set_testnet():
+    global net
+    net = BitcoinAtomTestnet
